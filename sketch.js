c5588b177a0c91144090fc7f1b88c530c381641b
danielkurtsawyer <19danielsawyer@gmail.com>
2023-07-25 22:07:02 +0800

    Implement createNewGrid()
    The completion of this function marks the end of the main part of this project.
    createNewGrid() simply creates a grid of squares and adds them to the grid div.

--- sketch.js
+++ sketch.js
@@ -1,8 +1,6 @@
 const grid = document.querySelector('.grid');
 const button = document.querySelector('button');
 
-button.addEventListener('click', changeNumberOfSquaresPerSide);
-
 const sheet = document.styleSheets[0];
 
 for(let i = 1; i <= Math.pow(16, 2); i++){
@@ -13,9 +11,13 @@ for(let i = 1; i <= Math.pow(16, 2); i++){
 }
 
 function changeNumberOfSquaresPerSide(){
-    let numSides = +(window.prompt('Enter the number of squares per side: '));
+    let numSides = window.prompt('Enter the number of squares per side: ');
 
+    // check if numSides is null before converting to a number because
+    // if the user clicks cancel, we don't want to clear the grid
     if(numSides === null) return;
+
+    numSides = +numSides;
 
     if(isNaN(numSides)){
         alert('Please enter a number');
@@ -29,7 +31,7 @@ function changeNumberOfSquaresPerSide(){
 
     clearGrid();
     editSquareRule(numSides);
-    // createNewGrid(numSides);
+    createNewGrid(numSides);
 }
 
 function clearGrid(){
@@ -41,13 +43,28 @@ function clearGrid(){
 }
 
 function editSquareRule(numSides){
+    // delete the rule at index 3 in the stylesheet, which is for the square class 
     sheet.deleteRule(3);
+    // create a new rule for the class square, updating the flex basis
     sheet.insertRule(`.square {flex: 1 1 ${90/numSides}vh; border: 1px solid black; box-sizing: border-box;}`, 3);
+}
+
+function createNewGrid(numSides){
+    for(let i = 1; i <= Math.pow(numSides, 2); i++){
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.addEventListener('mouseenter', hoverStart);
+        grid.appendChild(square);
+    }
 }
 
 function hoverStart(e){
     const square = e.target;
     square.classList.add('hover');
+
+   
 }
+
+
 
 
