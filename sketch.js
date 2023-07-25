8eed2715e222e16e73a2cb106458f9b64cd83987
danielkurtsawyer <19danielsawyer@gmail.com>
2023-07-25 20:58:03 +0800

    Add grid and hovering
    I added a 16x16 grid using javaScript and CSS.
    Additionally, I used JavaScript to create event listeners which change the background color of the inner squares when the mouse moves over them.

---
+++ sketch.js
@@ -0,0 +1,20 @@
+const grid = document.querySelector('.grid');
+
+for(let i = 1; i <= Math.pow(16, 2); i++){
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.addEventListener('mouseenter', hoverStart);
+    square.addEventListener('mouseleave', hoverEnd);
+    grid.appendChild(square);
+}
+
+function hoverStart(e){
+    const square = e.target;
+    square.classList.add('hover');
+}
+
+function hoverEnd(e){
+    const square = e.target;
+    square.classList.remove('hover');
+}
+
