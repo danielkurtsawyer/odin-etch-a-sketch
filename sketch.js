37b1a2e12c1690fc17a2a9be7bc445064a39a1b2
danielkurtsawyer <19danielsawyer@gmail.com>
2023-07-25 21:41:53 +0800

    Add button to change the number of squares in grid
    I added an event listener on the button which prompts the user to enter a number of squares per side.
    So far, I've only completed clearing the grid.

--- sketch.js
+++ sketch.js
@@ -1,11 +1,43 @@
 const grid = document.querySelector('.grid');
+const button = document.querySelector('button');
+
+button.addEventListener('click', changeNumberOfSquaresPerSide);
+
+const sheet = document.styleSheets[0];
 
 for(let i = 1; i <= Math.pow(16, 2); i++){
     const square = document.createElement('div');
     square.classList.add('square');
     square.addEventListener('mouseenter', hoverStart);
-    //square.addEventListener('mouseleave', hoverEnd);
     grid.appendChild(square);
+}
+
+function changeNumberOfSquaresPerSide(){
+    let numSides = +(window.prompt('Enter the number of squares per side: '));
+
+    if(numSides === null) return;
+
+    if(isNaN(numSides)){
+        alert('Please enter a number');
+        return changeNumberOfSquaresPerSide();
+    }
+
+    if(numSides > 100){
+        alert('The maximum value is 100');
+        return changeNumberOfSquaresPerSide();
+    }
+
+    clearGrid();
+    // editSquareRule(numSides);
+    // createNewGrid(numSides);
+}
+
+function clearGrid(){
+    let child = grid.firstChild;
+    while(child){
+        child.remove();
+        child = grid.firstChild;
+    }
 }
 
 function hoverStart(e){
@@ -13,9 +45,4 @@ function hoverStart(e){
     square.classList.add('hover');
 }
 
-/*
-function hoverEnd(e){
-    const square = e.target;
-    square.classList.remove('hover');
-}
-*/
+
